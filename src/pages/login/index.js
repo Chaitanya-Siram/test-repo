7cd3b33a2f10d96f9766f406a066b017eb746426
Chaitanya-Siram <siram.chaitanya@infovision.com>
2024-04-11 13:44:34 +0530

    Alphametricx

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -202,7 +202,7 @@ const LoginPage = () => {
               }}
             >
               <LoginFormBox>
-                <FormGraytxt>Login to AlphametricX</FormGraytxt>
+                <FormGraytxt>Login to Alphametricx</FormGraytxt>
                 <LoginForm onSubmit={submitLoginDetails}>
                   <InputBox
                     label="Registered Email"
