a3d45f0da429eae9a624e3d4704420ec2d3b87f0
Chaitanya-Siram <siram.chaitanya@infovision.com>
2024-04-17 16:17:03 +0530

    Title name changed

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -202,7 +202,7 @@ const LoginPage = () => {
               }}
             >
               <LoginFormBox>
-                <FormGraytxt>Login to Alphametric</FormGraytxt>
+                <FormGraytxt>Login to Alphametricx</FormGraytxt>
                 <LoginForm onSubmit={submitLoginDetails}>
                   <InputBox
                     label="Registered Email"
